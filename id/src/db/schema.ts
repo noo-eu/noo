fd0f90e644dd1b67a85048c001bf554a141854ea
Francesco Boffa <hello@noo.eu>
2025-02-27 18:24:18 +0100

    Create OIDC clients schema

--- id/src/db/schema.ts
+++ id/src/db/schema.ts
@@ -1,5 +1,8 @@
 import {
+  boolean,
   inet,
+  integer,
+  jsonb,
   pgTable,
   text,
   timestamp,
@@ -48,4 +51,52 @@ export const sessions = pgTable("sessions", {
   userAgent: text("user_agent"),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   lastUsedAt: timestamp("last_used_at").notNull().defaultNow(),
+});
+
+export const oidcClients = pgTable("oidc_clients", {
+  id: uuid().primaryKey().defaultRandom(),
+  tenantId: uuid("tenant_id").references(() => tenants.id, {
+    onDelete: "cascade",
+    onUpdate: "cascade",
+  }),
+  clientId: text("client_id").notNull(),
+  clientSecret: text("client_secret").notNull(),
+  redirectUris: text("redirect_uris").array().notNull(),
+  responseTypes: text("response_types").array().notNull().default(["code"]),
+  grantTypes: text("grant_types")
+    .array()
+    .notNull()
+    .default(["authorization_code"]),
+  applicationType: text("application_type").notNull().default("web"),
+  contacts: text("contacts").array(),
+  clientName: jsonb("client_name"),
+  logoUri: jsonb("logo_uri"),
+  clientUri: jsonb("client_uri"),
+  policyUri: jsonb("policy_uri"),
+  tosUri: jsonb("tos_uri"),
+  jwksUri: text("jwks_uri"),
+  jwks: jsonb("jwks"),
+  sectorIdentifierUri: text("sector_identifier_uri"),
+  subjectType: text("subject_type").notNull().default("pairwise"),
+  idTokenSignedResponseAlg: text("id_token_signed_response_alg")
+    .notNull()
+    .default("RS256"),
+  idTokenEncryptedResponseAlg: text("id_token_encrypted_response_alg"),
+  idTokenEncryptedResponseEnc: text("id_token_encrypted_response_enc"),
+  userinfoSignedResponseAlg: text("userinfo_signed_response_alg"),
+  userinfoEncryptedResponseAlg: text("userinfo_encrypted_response_alg"),
+  userinfoEncryptedResponseEnc: text("userinfo_encrypted_response_enc"),
+  requestObjectSigningAlg: text("request_object_signing_alg"),
+  requestObjectEncryptionAlg: text("request_object_encryption_alg"),
+  requestObjectEncryptionEnc: text("request_object_encryption_enc"),
+  tokenEndpointAuthMethod: text("token_endpoint_auth_method")
+    .notNull()
+    .default("client_secret_basic"),
+  tokenEndpointAuthSigningAlg: text("token_endpoint_auth_signing_alg"),
+  defaultMaxAge: integer("default_max_age"),
+  requireAuthTime: boolean("require_auth_time").notNull().default(false),
+  defaultAcrValues: text("default_acr_values").array(),
+  initiateLoginUri: text("initiate_login_uri"),
+  requestUris: text("request_uris").array(),
+  postLogoutRedirectUris: text("post_logout_redirect_uris").array(),
 });
