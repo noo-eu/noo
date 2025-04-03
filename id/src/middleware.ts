c102d62853274a294398157afadd3305b595fd14
Francesco Boffa <hello@noo.eu>
2025-04-03 18:27:53 +0200

    Allow static for img and fonts

--- id/src/middleware.ts
+++ id/src/middleware.ts
@@ -21,8 +21,8 @@ export async function middleware(request: NextRequest) {
     default-src 'self' https://static.noo.eu;
     script-src 'nonce-${nonce}' 'strict-dynamic' 'unsafe-inline' https:;
     style-src 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self';
+    img-src 'self' https://static.noo.eu blob: data:;
+    font-src 'self' https://static.noo.eu;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
