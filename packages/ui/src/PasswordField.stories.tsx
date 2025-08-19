b0b58187eefdf10737d8c4b4f8f46b1db6b16417
Francesco Boffa <hello@noo.eu>
2025-08-19 18:50:26 +0200

    Vite upgrade

--- packages/ui/src/PasswordField.stories.tsx
+++ packages/ui/src/PasswordField.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { IntlProvider } from "use-intl";
 import { PasswordField } from "./PasswordField";
